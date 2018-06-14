719eceabd708c5445be01b226d480a10bc340b09
Sergio Souza <emailsouza@gmail.com>
2018-06-14 16:48:28 -0600

    Commit 2 - Palindrome

--- solutions.js
+++ solutions.js
@@ -6,3 +6,16 @@
 // console.log prints to the console
 // return returns the value (can be used by other functions, or calls)
 
+// Write a function checkPalindrome that accepts a single argument, a string.The function should return true(Boolean) if the string is a palindrome, false if it is not.Make sure your function will give the correct answer for words with capital letters.
+
+const checkPalindrome = (string) => {
+  let reverseString = string.split('').reverse('').join('');
+  if (string.toLowerCase() === reverseString.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(checkPalindrome("Radar"));
+console.log(checkPalindrome("Borscht"));
